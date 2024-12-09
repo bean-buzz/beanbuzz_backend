b39af8f07576baf23800335d4295d278924cfcab
Jason Parkin <jay-parkin91@hotmail.com>
2024-12-09 19:56:04 +1100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const jwt = require("jsonwebtoken");
 const cors = require("cors");
 
 const app = express();
@@ -8,6 +7,7 @@ const app = express();
 app.use(
   cors({
     origin: [
+      "http://127.0.0.1:5173",
       "http://localhost:3000",
       "http://localhost:8080",
       "http://localhost:5173",
