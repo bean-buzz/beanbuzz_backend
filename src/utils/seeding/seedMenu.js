2380869d232dfc7bc022c9e7b04d804080e44893
rday45 <rahularvinddayal@gmail.com>
2024-12-15 18:09:22 +1100

    attempting to create seeding function but a few bugs are present

--- src/utils/seeding/seedMenu.js
+++ src/utils/seeding/seedMenu.js
@@ -1,25 +1,47 @@
 const mongoose = require("mongoose");
-const { dbConnect } = require("../../functions/dbFunctions.js");
-const dotenv = require("dotenv");
+const { dbConnect, dbClose } = require("../../functions/dbFunctions.js");
+const { MenuItem } = require("../../models/MenuModel.js");
 const fs = require("fs");
 const path = require("path");
-dotenv.config();
+require("dotenv").config();
 
-const dbPath = path.join(__dirname, "menu-items.json");
+const jsonFilePath = path.join(__dirname, "menu-items.json");
 
-async function jsonData() {
+async function retrieveMenuItemData() {
   try {
-    const data = await fs.readFileSync(dbPath, "utf-8");
-    return JSON.parse(data);
+    const data = await fs.readFileSync(jsonFilePath, "utf-8");
+    const menuItemData = await JSON.parse(data).menuItems;
+    return menuItemData;
   } catch (error) {
-    console.log("Error reading or parsing json objects", error);
+    console.log(
+      "Error reading or parsing json objects when retrieving json data",
+      error
+    );
     return null;
   }
 }
+// // The seedMenuData function works when i hard code the url. But doesn't work when i import dbConnect and use that. Need to look into this
+// async function dbConnectHardCoded() {
+//   let databaseURL = `mongodb://127.0.0.1:27017/beanbuzz_backend`;
+//   try {
+//     await mongoose.connect(databaseURL);
+//     console.log("Connected to database at " + databaseURL);
+//   } catch (error) {
+//     console.log(`dbConnect failed! error: \n ${JSON.stringify(error)}`);
+//   }
+// }
 
-async function logData() {
-  const jsonObjects = await jsonData();
-  console.log(jsonData.menuItems);
+// function not working as intended. Can't connect to database
+async function seedMenuData() {
+  const sampleMenuItemData = await retrieveMenuItemData();
+  try {
+    // doesn't seem to work with dbConnect(); Last part of url is undefined
+    await dbConnect();
+    await MenuItem.insertMany(sampleMenuItemData);
+    await dbClose();
+  } catch (error) {
+    console.log("Error seeding menu data.", error);
+  }
 }
 
-logData();
+seedMenuData();
